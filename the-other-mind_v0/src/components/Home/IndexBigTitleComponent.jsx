8571fbf81e8553b64e6deb8aecd8629c948bd516
alexandreb42 <alexandre.bloch@epita.fr>
2020-03-16 20:10:31 +0100

    creation serveur + maj

--- the-other-mind_v0/src/components/Home/IndexBigTitleComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useContext, useState } from "react"
-
-export default function IndexBigTitleComponent({gameModules}) {
-    return(
-        <>
-            <h1>The Other Mind</h1>
-        </>
-    )
-}
--- the-other-mind_v0/src/constants/actionTypes.js
+++
@@ -1,3 +0,0 @@
-export const LOAD_DATA = 'LOAD_DATA';
-export const SUCCESS_DATA = 'SUCCESS_DATA';
-export const ERROR_DATA = 'ERROR_DATA';
