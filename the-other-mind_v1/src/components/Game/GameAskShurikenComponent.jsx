d510b25c46cdebf055b631f16e0e0550594a9624
Elvan Eroglu <elvan.alwan@gmail.com>
2020-09-01 16:52:25 +0200

    dashboard game

--- the-other-mind_v1/src/components/Game/GameAskShurikenComponent.jsx
+++ the-other-mind_v1/src/components/Game/GameAskShurikenComponent.jsx
@@ -1,14 +1,16 @@
-import React, { useContext, useState } from "react"
+import React, { useContext, useState } from "react";
 
-export default function GameAskShurikenComponent({gameModules}) {
-    return(
-        <>
-        <p>Demander un Shuriken</p>
-        <ul>
-            {gameModules && gameModules.map(module2 => (
-                <li>{module2.name}</li>
-            ))}
-        </ul>
-        </>
-    )
+export default function GameAskShurikenComponent({ gameModules }) {
+  const askShuriken = () => {
+    console.log("method demander un shuriken");
+  };
+  return (
+    <>
+      <p>Demander un Shuriken</p>
+      <ul>
+        {gameModules && gameModules.map((module2) => <li>{module2.name}</li>)}
+      </ul>
+      <button onClick={askShuriken}>Demander un Shuriken</button>
+    </>
+  );
 }
--- the-other-mind_v1/src/components/Game/GamePlayerOneComponent.jsx
+++ the-other-mind_v1/src/components/Game/GamePlayerOneComponent.jsx
@@ -1,14 +1,15 @@
-import React, { useContext, useState } from "react"
+import React, { useContext, useState } from "react";
 
-export default function GamePlayerOneComponent({cards}) {
-    return(
-        <>
-        <p>Cartes du joueur</p>
-        <ul>
-            {cards && cards.map(module2 => (
-                <li>{module2.name}</li>
-            ))}
-        </ul>
-        </>
-    )
+export default function GamePlayerOneComponent({ cards }) {
+  const playACard = () => {
+    console.log("jouer une carte");
+  };
+
+  return (
+    <>
+      <p>Cartes du joueur</p>
+      <ul>{cards && cards.map((module2) => <li>{module2.name}</li>)}</ul>
+      <button onClick={playACard}>Jouer une carte</button>
+    </>
+  );
 }
