2861cae6fd68d4cee4cd87166c96691046989b2a
Elvan Eroglu <elvan.alwan@gmail.com>
2020-09-07 03:09:53 +0200

    comment

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,9 @@ io.on("connection", (socket) => {
 
 const getApiAndEmit = (socket) => {
   const response = new Date();
+  const message = "coucou";
   // Emitting a new message. Will be consumed by the client
+  //   socket.emit("FromAPI", response);
   socket.emit("FromAPI", response);
 };
 
--- the-other-mind_v1/src/App.js
+++ the-other-mind_v1/src/App.js
@@ -12,10 +12,17 @@ import Routes from "./Routes";
 const token = localStorage.IdToken;
 if (token) {
   const decodedToken = jwtDecode(token);
+  // si le token existe on le décode
   if (decodedToken.exp * 1000 < Date.now()) {
+    // le token a une durée de vie de 24h (défini dans le backend)
+    // au delà de 24h le token remis à une date normal (avec decodedToken.exp * 1000 pour les millisecondes) ne sera plus égal au moment présent
+    // il aura expiré
+    // en cas d'expiration on déconnecte l'user
     store.dispatch(logoutUser());
+    // et on le renvoie à la page de connexion
     window.location.href = "/login";
   } else {
+    // autrement, on dispatch une action de type SET_AUTHENTICATED qui aura pour effet d'enregistrer le token à travers le local storage
     store.dispatch({ type: SET_AUTHENTICATED });
     axios.defaults.headers.common["Authorization"] = token;
     store.dispatch(getUserData());
